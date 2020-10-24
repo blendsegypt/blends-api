7f39693d273651afe86d4138941ab2a38241e7b1
Khalid Khalil <coderizer@aol.com>
2020-10-24 22:04:08 +0200

    Remove default index router

--- app.js
+++ app.js
@@ -3,8 +3,6 @@ var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 
-var indexRouter = require("./routes/index");
-
 var app = express();
 
 app.use(logger("dev"));
