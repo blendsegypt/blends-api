69a9153d9fc5146b04f10003108f0a2530d9bd39
Khalid Khalil <coderizer@aol.com>
2020-10-25 22:07:27 +0200

    Restructuring folder structure and add comments for readability

--- app.js
+++ app.js
@@ -1,16 +1,27 @@
-// var express = require("express");
-import Express from "express";
-import Path from "path";
-import CookieParser from "cookie-parser";
-import Logger from "morgan";
-import db from "./models/index";
+import Express from "express"; // Express
+import Path from "path"; // Path to enable path methods
+import CookieParser from "cookie-parser"; // Cookie Parser to parse cookies from HTTP requests
+import Logger from "morgan"; // Morgan Logger for debugging
+//Controller import example:
+//import UserController from "./controllers/user";
 
+//Initialize Express Application
 var App = Express();
 
+// Apply Middleware
 App.use(Logger("dev"));
 App.use(Express.json());
 App.use(Express.urlencoded({ extended: false }));
 App.use(CookieParser());
 App.use(Express.static(Path.join(__dirname, "public")));
+
+/*
+ *
+ *  API Routes
+ *
+ */
+
+//Controller usage example:
+//App.use("/users", UserController);
 
 export default App;
