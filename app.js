3edcc061c4c433c691f40816b15797be931621e1
Khalid Khalil <coderizer@aol.com>
2020-10-25 20:50:37 +0200

    Testing Database Connection

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+const { Sequelize } = require("sequelize");
 
 var app = express();
 
@@ -10,6 +11,20 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+
+const sequelize = new Sequelize("blends", "root", "", {
+  host: "localhost",
+  dialect: "postgres",
+});
+
+(async function () {
+  try {
+    await sequelize.authenticate();
+    console.log("Connection has been established successfully.");
+  } catch (error) {
+    console.error("Unable to connect to the database:", error);
+  }
+})();
 
 app.get("/", (req, res) => {
   res.send("Hello Worldzzz!");
