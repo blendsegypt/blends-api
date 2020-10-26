a422c4180943e233fb2d0cba32026acf5ed509a9
AlyMBarakat <aly.m.barakat@gmail.com>
2020-10-26 20:00:09 +0200

    Create User model

---
+++ models/user.js
@@ -0,0 +1,37 @@
+import { DataTypes } from "sequelize";
+
+export default User = sequelize.define('User', {
+  // Model attributes are defined here
+  firstName: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  lastName: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  phone_number: {
+    type: DataTypes.INTEGER,
+    email: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+  },
+  email_verified: DataTypes.BOOLEAN,
+  gender: DataTypes.ENUM('Male', 'Female', 'Prefer not to tell'),
+  dob: DataTypes.DATE,
+  password_hash: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  password_salt: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true
+  },
+  platform: {
+    type: DataTypes.ENUM('ios', 'android', 'other'),
+    allowNull: false,
+  }
+}, {
+  // Other model options go here
+});
