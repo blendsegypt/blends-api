866ddca89d44da994ffa248904d88e3c8e8a8a5b
AlyMBarakat <aly.m.barakat@gmail.com>
2020-10-27 00:53:53 +0200

    Change Model structure to working with sequelize-auto-migrations

--- models/user.js
+++ models/user.js
@@ -1,37 +1,46 @@
-import { DataTypes } from "sequelize";
+module.exports = function (sequelize, DataTypes) {
+  return sequelize.define('User', {
+    // Model attributes are defined here
+    firstName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    lastName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    phone_number: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      unique: true,
+    },
+    email: {
+      type: DataTypes.STRING,
+    },
+    email_verified: {
+      type: DataTypes.BOOLEAN,
+    },
+    gender: {
+      type: DataTypes.ENUM('Male', 'Female', 'Prefer not to tell'),
+    },
+    dob: {
+      type: DataTypes.DATE
+    },
+    password_hash: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    password_salt: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    platform: {
+      type: DataTypes.ENUM('ios', 'android', 'other'),
+      allowNull: false,
+    }
+  }, {
+    // Other model options go here
+  });
 
-export default User = sequelize.define('User', {
-  // Model attributes are defined here
-  firstName: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  lastName: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  phone_number: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-    unique: true,
-  },
-  email: DataTypes.STRING,
-  email_verified: DataTypes.BOOLEAN,
-  gender: DataTypes.ENUM('Male', 'Female', 'Prefer not to tell'),
-  dob: DataTypes.DATE,
-  password_hash: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  password_salt: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    unique: true
-  },
-  platform: {
-    type: DataTypes.ENUM('ios', 'android', 'other'),
-    allowNull: false,
-  }
-}, {
-  // Other model options go here
-});
+}
