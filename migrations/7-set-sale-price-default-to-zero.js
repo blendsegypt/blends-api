ad069f2c25b9cdc9750b9fef027928c3795de660
Khalid Khalil <coderizer@aol.com>
2020-10-31 20:18:33 +0200

    Fix a bug at auto-generated migration

--- migrations/7-set-sale-price-default-to-zero.js
+++ migrations/7-set-sale-price-default-to-zero.js
@@ -1,6 +1,6 @@
-'use strict';
+"use strict";
 
-var Sequelize = require('sequelize');
+var Sequelize = require("sequelize");
 
 /**
  * Actions summary:
@@ -10,82 +10,82 @@ var Sequelize = require('sequelize');
  **/
 
 var info = {
-    "revision": 7,
-    "name": "set-sale-price-default-to-zero",
-    "created": "2020-10-31T18:17:27.314Z",
-    "comment": ""
+  revision: 7,
+  name: "set-sale-price-default-to-zero",
+  created: "2020-10-31T18:17:27.314Z",
+  comment: "",
 };
 
-var migrationCommands = function(transaction) {
-    return [{
-        fn: "changeColumn",
-        params: [
-            "Products",
-            "sale_price",
-            {
-                "type": Sequelize.DOUBLE PRECISION,
-                "field": "sale_price",
-                "default": 0
-            },
-            {
-                transaction: transaction
-            }
-        ]
-    }];
+var migrationCommands = function (transaction) {
+  return [
+    {
+      fn: "changeColumn",
+      params: [
+        "Products",
+        "sale_price",
+        {
+          type: Sequelize.DOUBLE,
+          field: "sale_price",
+          default: 0,
+        },
+        {
+          transaction: transaction,
+        },
+      ],
+    },
+  ];
 };
-var rollbackCommands = function(transaction) {
-    return [{
-        fn: "changeColumn",
-        params: [
-            "Products",
-            "sale_price",
-            {
-                "type": Sequelize.DOUBLE PRECISION,
-                "field": "sale_price"
-            },
-            {
-                transaction: transaction
-            }
-        ]
-    }];
+var rollbackCommands = function (transaction) {
+  return [
+    {
+      fn: "changeColumn",
+      params: [
+        "Products",
+        "sale_price",
+        {
+          type: Sequelize.DOUBLE,
+          field: "sale_price",
+        },
+        {
+          transaction: transaction,
+        },
+      ],
+    },
+  ];
 };
 
 module.exports = {
-    pos: 0,
-    useTransaction: true,
-    execute: function(queryInterface, Sequelize, _commands)
-    {
-        var index = this.pos;
-        function run(transaction) {
-            const commands = _commands(transaction);
-            return new Promise(function(resolve, reject) {
-                function next() {
-                    if (index < commands.length)
-                    {
-                        let command = commands[index];
-                        console.log("[#"+index+"] execute: " + command.fn);
-                        index++;
-                        queryInterface[command.fn].apply(queryInterface, command.params).then(next, reject);
-                    }
-                    else
-                        resolve();
-                }
-                next();
-            });
+  pos: 0,
+  useTransaction: true,
+  execute: function (queryInterface, Sequelize, _commands) {
+    var index = this.pos;
+    function run(transaction) {
+      const commands = _commands(transaction);
+      return new Promise(function (resolve, reject) {
+        function next() {
+          if (index < commands.length) {
+            let command = commands[index];
+            console.log("[#" + index + "] execute: " + command.fn);
+            index++;
+            queryInterface[command.fn]
+              .apply(queryInterface, command.params)
+              .then(next, reject);
+          } else resolve();
         }
-        if (this.useTransaction) {
-            return queryInterface.sequelize.transaction(run);
-        } else {
-            return run(null);
-        }
-    },
-    up: function(queryInterface, Sequelize)
-    {
-        return this.execute(queryInterface, Sequelize, migrationCommands);
-    },
-    down: function(queryInterface, Sequelize)
-    {
-        return this.execute(queryInterface, Sequelize, rollbackCommands);
-    },
-    info: info
+        next();
+      });
+    }
+    if (this.useTransaction) {
+      return queryInterface.sequelize.transaction(run);
+    } else {
+      return run(null);
+    }
+  },
+  up: function (queryInterface, Sequelize) {
+    return this.execute(queryInterface, Sequelize, migrationCommands);
+  },
+  down: function (queryInterface, Sequelize) {
+    return this.execute(queryInterface, Sequelize, rollbackCommands);
+  },
+  info: info,
 };
