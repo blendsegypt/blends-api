295a580544e7de43f7169aa98a541d9149817281
Khalid Khalil <coderizer@aol.com>
2020-11-09 17:31:50 +0200

    Add imports for new cron job

--- app.js
+++ app.js
@@ -3,10 +3,11 @@ import Path from "path"; // Path to enable path methods
 import CookieParser from "cookie-parser"; // Cookie Parser to parse cookies from HTTP requests
 import Logger from "morgan"; // Morgan Logger for debugging
 import cors from "cors"; //Allow cross-origin requests
+import expiredShipmentsCronJob from "./cron/expiredShipments";
 
 // import routes
 import appRoutes from "./routes/app";
-import adminRoutes from "./routes/admin"
+import adminRoutes from "./routes/admin";
 
 //Initialize Express Application
 var App = Express();
@@ -18,7 +19,6 @@ App.use(Express.urlencoded({ extended: false }));
 App.use(CookieParser());
 App.use(Express.static(Path.join(__dirname, "public")));
 App.use(cors());
-
 
 // use routes
 App.use("/app", appRoutes);
