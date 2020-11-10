40f7c28c45af47fa99223ebda67d07b38cde4e20
Khalid Khalil <coderizer@aol.com>
2020-11-10 13:37:14 +0200

    Add Order Model

---
+++ models/order.js
@@ -0,0 +1,74 @@
+module.exports = function (sequelize, DataTypes) {
+  const Order = sequelize.define(
+    "Order",
+    {
+      // Model attributes are defined here
+      preparing_at: {
+        type: DataTypes.DATE,
+      },
+      delivering_at: {
+        type: DataTypes.DATE,
+      },
+      delivered_at: {
+        type: DataTypes.DATE,
+      },
+      order_number: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      order_status: {
+        type: DataTypes.ENUM(
+          "Received",
+          "Preparing",
+          "Delivering",
+          "Delivered"
+        ),
+        allowNull: false,
+      },
+      delivery_charges: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      sub_total: {
+        type: DataTypes.DOUBLE,
+        allowNull: false,
+      },
+      total: {
+        type: DataTypes.DOUBLE,
+        allowNull: false,
+      },
+      total_after_promo: {
+        type: DataTypes.DOUBLE,
+      },
+      assigned_driver: {
+        type: DataTypes.STRING,
+      },
+    },
+    {
+      // Other model options go here
+    }
+  );
+
+  Order.associate = (models) => {
+    // Orders belongs to a user
+    Order.belongsTo(models.User, {
+      foreignKey: {
+        name: "user_id",
+        allowNull: false,
+      },
+      onDelete: "cascade",
+    });
+    models.User.hasMany(Order, { foreignKey: "user_id" });
+    // Orders belong to a branch
+    Order.belongsTo(models.Branch, {
+      foreignKey: {
+        name: "branch_id",
+        allowNull: false,
+      },
+      onDelete: "cascade",
+    });
+    models.Branch.hasMany(Order, { foreignKey: "branch_id" });
+  };
+
+  return Order;
+};
