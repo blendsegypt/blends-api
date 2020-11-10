98ee0fc896afc3cbb589829841061a0afdb933b8
Khalid Khalil <coderizer@aol.com>
2020-11-10 13:38:01 +0200

    Add OrderItem model

---
+++ models/orderItem.js
@@ -0,0 +1,31 @@
+module.exports = function (sequelize, DataTypes) {
+  const OrderItem = sequelize.define(
+    "OrderItem",
+    {
+      // Model attributes are defined here
+      quantity: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      options: {
+        type: DataTypes.STRING,
+      },
+    },
+    {
+      // Other model options go here
+    }
+  );
+
+  OrderItem.associate = (models) => {
+    OrderItem.belongsTo(models.Order, {
+      foreignKey: {
+        name: "order_id",
+        allowNull: false,
+      },
+      onDelete: "cascade",
+    });
+    models.Order.hasMany(OrderItem, { foreignKey: "order_id" });
+  };
+
+  return OrderItem;
+};
