9b288a9f01a07d215fc5b1ba18b1d5cc3a7c8e5f
Khalid Khalil <coderizer@aol.com>
2020-11-12 19:09:05 +0200

    Add Users App controller route

--- routes/app.js
+++ routes/app.js
@@ -4,9 +4,11 @@ const router = Express.Router();
 // import app controllers
 import ProductAsUser from "../controllers/productAsUser"; // app
 import ApplyPromoCodes from "../controllers/applyPromoCodes"; // app
+import Users from "../controllers/usersAsApp";
 
 // route controllers
 router.use("/products", ProductAsUser);
 router.use("/apply-promo-code", ApplyPromoCodes);
+router.use("/users", Users);
 
 export default router;
