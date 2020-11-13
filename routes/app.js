8b663c5b4e88da8debc97bd35844e23f48415fd2
Khalid Khalil <coderizer@aol.com>
2020-11-13 20:54:34 +0200

    Add authenticated endpoint example

--- routes/app.js
+++ routes/app.js
@@ -1,14 +1,19 @@
 import Express from "express";
 const router = Express.Router();
 
+//Auth Middleware
+import authenticate from "../middleware/authenticate";
+
 // import app controllers
 import ProductAsUser from "../controllers/productAsUser"; // app
 import ApplyPromoCodes from "../controllers/applyPromoCodes"; // app
-import Users from "../controllers/usersAsApp";
+import Register from "../controllers/register";
+import UserLogin from "../controllers/userLogin";
 
 // route controllers
-router.use("/products", ProductAsUser);
+router.use("/products", authenticate, ProductAsUser);
 router.use("/apply-promo-code", ApplyPromoCodes);
-router.use("/users", Users);
+router.use("/register", Register);
+router.use("/auth", UserLogin);
 
 export default router;
