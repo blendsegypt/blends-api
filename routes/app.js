d900ffdd38234c2288ac7ece2b89e780106d3209
Khalid Khalil <coderizer@aol.com>
2020-11-14 16:35:39 +0200

    Add refresh token endpoint controller route

--- routes/app.js
+++ routes/app.js
@@ -9,11 +9,13 @@ import ProductAsUser from "../controllers/productAsUser"; // app
 import ApplyPromoCodes from "../controllers/applyPromoCodes"; // app
 import Register from "../controllers/register";
 import UserLogin from "../controllers/userLogin";
+import RefreshToken from "../controllers/refreshToken";
 
 // route controllers
 router.use("/products", authenticate, ProductAsUser);
 router.use("/apply-promo-code", ApplyPromoCodes);
 router.use("/register", Register);
-router.use("/auth", UserLogin);
+router.use("/auth/login", UserLogin);
+router.use("/auth/refresh", RefreshToken);
 
 export default router;
