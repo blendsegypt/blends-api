44bcf139f4e03d793fe2379bc4d202ec6363bb66
Khalid Khalil <coderizer@aol.com>
2020-11-15 16:31:51 +0200

    Associate Promocode with Product

--- models/promoCode.js
+++ models/promoCode.js
@@ -1,49 +1,63 @@
 module.exports = function (sequelize, DataTypes) {
-    const PromoCode = sequelize.define(
-        'PromoCode',
-        {
-            // Model attributes are defined here
-            code: {
-                type: DataTypes.STRING,
-                allowNull: false,
-                unique: true,
-            },
-            type: {
-                type: DataTypes.ENUM("percentage", "fixed", "free_delivery", "free_item", "cashback"),
-                allowNull: false,
-            },
-            start_date: {
-                type: DataTypes.DATE,
-            },
-            end_date: {
-                type: DataTypes.DATE,
-            },
-            max_usage_per_user: {
-                type: DataTypes.INTEGER,
-                defaultValue: 1,
-            },
-            min_order_value: {
-                type: DataTypes.DOUBLE,
-                defaultValue: 0.0,
-            },
-            percentage_discount: {
-                type: DataTypes.INTEGER,
-                defaultValue: 0,
-            },
-            free_product: { // product_id
-                type: DataTypes.INTEGER,
-            },
-            cashback_amount: {
-                type: DataTypes.DOUBLE,
-                defaultValue: 0.0,
-            },
-            fixed_amount: {
-                type: DataTypes.DOUBLE,
-                defaultValue: 0.0,
-            }
-        }, {
-        // Other model options go here
-    });
+  const PromoCode = sequelize.define(
+    "PromoCode",
+    {
+      // Model attributes are defined here
+      code: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      type: {
+        type: DataTypes.ENUM(
+          "percentage",
+          "fixed",
+          "free_delivery",
+          "free_item",
+          "cashback"
+        ),
+        allowNull: false,
+      },
+      start_date: {
+        type: DataTypes.DATE,
+      },
+      end_date: {
+        type: DataTypes.DATE,
+      },
+      max_usage_per_user: {
+        type: DataTypes.INTEGER,
+        defaultValue: 1,
+      },
+      min_order_value: {
+        type: DataTypes.DOUBLE,
+        defaultValue: 0.0,
+      },
+      percentage_discount: {
+        type: DataTypes.INTEGER,
+        defaultValue: 0,
+      },
+      cashback_amount: {
+        type: DataTypes.DOUBLE,
+        defaultValue: 0.0,
+      },
+      fixed_amount: {
+        type: DataTypes.DOUBLE,
+        defaultValue: 0.0,
+      },
+    },
+    {
+      // Other model options go here
+    }
+  );
 
-    return PromoCode;
+  PromoCode.associate = (models) => {
+    PromoCode.belongsTo(models.Product, {
+      foreignKey: {
+        name: "free_product",
+      },
+    });
+    models.Product.hasMany(PromoCode, { foreignKey: "free_product" });
+  };
+
+  return PromoCode;
 };
