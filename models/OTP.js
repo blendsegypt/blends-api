cfcca21ba8c4669a101318626cf06b2ca842008d
Khalid Khalil <coderizer@aol.com>
2020-11-16 21:33:13 +0200

    Change OTP field fromInteger to String

--- models/OTP.js
+++ models/OTP.js
@@ -8,7 +8,7 @@ module.exports = function (sequelize, DataTypes) {
         allowNull: false,
       },
       OTP: {
-        type: DataTypes.INTEGER,
+        type: DataTypes.STRING,
         allowNull: false,
       },
       verified: {
