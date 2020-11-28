6782bf242105b112088a6a08d48988b25544a4ca
Khalid Khalil <coderizer@aol.com>
2020-11-28 17:54:07 +0200

    Retrieve retail products stock according to branch for app homescreen

--- controllers/app/Products.js
+++ controllers/app/Products.js
@@ -18,8 +18,8 @@ router.get("/categories", async (req, res) => {
   }
 });
 
-//get products by category (Homescreen in mobile application)
-router.get("/categories/:category_id", async (req, res) => {
+//get products by category & branch (Homescreen in mobile application)
+router.get("/category/:category_id/branch/:branch_id", async (req, res) => {
   try {
     const category_id = req.params.category_id;
     const productsFromCategory = await DB.Product.findAll({
@@ -34,6 +34,16 @@ router.get("/categories/:category_id", async (req, res) => {
         "sale_price",
         "retail",
         "product_image_url",
+      ],
+      include: [
+        {
+          required: false,
+          model: DB.Inventory,
+          attributes: ["actual_stock", "branch_id"],
+          where: {
+            branch_id: req.params.branch_id,
+          },
+        },
       ],
     });
     res.status(200).json({
